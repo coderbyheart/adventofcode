8463e0bffe206cac9e5f8dd8fa561b56a0ab87fb
Markus Tacker <m@coderbyheart.com>
2018-12-02 23:58:38 +0100

    test: add test for repeatedFrequency

--- 2018/day1.spec.js
+++ 2018/day1.spec.js
@@ -2,6 +2,12 @@
 
 const {calibrate} = require('./day1')
 
+function* listLooper (list) {
+    var index = 0;
+    while (true)
+        yield list[index++ % list.length];
+}
+
 describe('device calibration', () => {
     it('should calculate the right frequency', () => {
         expect(calibrate([
@@ -10,6 +16,28 @@ describe('device calibration', () => {
         expect(calibrate([1, 1, 1])).toEqual(3)
         expect(calibrate([1, 1, -2])).toEqual(0)
         expect(calibrate([-1, -2, -3])).toEqual(-6)
+    })
+
+    it('should find the frequency which it reaches twice', () => {
+        expect(repeatedFrequency(listLooper([
+            1, -2, 3, 1
+        ]))).toEqual(2)
+
+        expect(repeatedFrequency(listLooper([
+            +1, -1
+        ]))).toEqual(0)
+
+        expect(repeatedFrequency(listLooper([
+            +3, +3, +4, -2, -4
+        ]))).toEqual(10)
+
+        expect(repeatedFrequency(listLooper([
+            -6, +3, +8, +5, -6
+        ]))).toEqual(5)
+
+        expect(repeatedFrequency(listLooper([
+            +7, +7, -2, -7, -4
+        ]))).toEqual(14)
     })
 
     it('should calculate the solution', () => {
