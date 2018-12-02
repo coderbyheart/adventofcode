b6dda528c957648ad0616603ef3e0db2e7ecdc54
Markus Tacker <m@coderbyheart.com>
2018-12-03 00:12:45 +0100

    day 1 puzzle 2

--- 2018/day1.js
+++ 2018/day1.js
@@ -1,5 +1,20 @@
 'use strict'
 
+const calibrate = changes => changes.reduce((frequencies, frequency) => frequencies + frequency, 0)
+
+const repeatedFrequency = (frequencyGenerator, limit) => {
+    const seenFrequencies = {0: true}
+    const seenChanges = []
+    do {
+        seenChanges.push(frequencyGenerator.next().value)
+        const frequency = calibrate(seenChanges)
+        if (seenFrequencies[frequency]) return frequency
+        seenFrequencies[frequency] = true
+    } while (true)
+}
+
+
 module.exports = {
-    calibrate: changes => changes.reduce((frequencies, frequency) => frequencies + frequency, 0)
+    calibrate,
+    repeatedFrequency
 }
