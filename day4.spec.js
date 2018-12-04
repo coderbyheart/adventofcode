94bac59f30c873c92152121ad7476c4dcd9b39fd
Markus Tacker <m@coderbyheart.com>
2018-12-04 23:48:39 +0100

    feat: day 4 part 2

--- day4.spec.js
+++ day4.spec.js
@@ -61,7 +61,7 @@ const guardReport = logs => {
 const findSleepyGuard = guards => {
   const guardSleeps = Object.keys(guards).map(k => ({
     id: k,
-    sleepMinutes: guards[k].sleepMinutes,
+    sleepMinutes: guards[k].sleepMinutes
   }))
   const mostASleep = guardSleeps.sort(({ sleepMinutes: m1 }, { sleepMinutes: m2 }) => m2 - m1)
   const id = +mostASleep[0].id
@@ -80,24 +80,44 @@ const findSleepyGuard = guards => {
   }
 }
 
+const findFrequentGuard = guards => {
+  const guardSleeps = Object.keys(guards).map(k => ({
+    id: k,
+    asleepAt: guards[k].asleepAt.reduce((freq, count, minute) => {
+      if (count > freq.count) {
+        freq.count = count
+        freq.minute = minute
+      }
+      return freq
+    }, { minute: 0, count: 0 })
+  }))
+  const mostFrequent = guardSleeps.sort(({ asleepAt: a1 }, { asleepAt: a2 }) => a2.count - a1.count)
+  const id = +mostFrequent[0].id
+  const highestMinute = +mostFrequent[0].asleepAt.minute
+  return {
+    mostFrequent: id,
+    highestMinute
+  }
+}
+
 describe('find the sleepy guard', () => {
   it('report the log', () => {
     expect(guardReport([
       '[1518-11-01 00:00] Guard #10 begins shift',
       '[1518-11-01 00:01] falls asleep',
-      '[1518-11-01 00:02] wakes up',
+      '[1518-11-01 00:02] wakes up'
     ])).toEqual({ '10': { 'asleepAt': [undefined, 1], 'sleepMinutes': 1 } })
     expect(guardReport([
       '[1518-11-01 00:00] Guard #10 begins shift',
       '[1518-11-01 00:01] falls asleep',
-      '[1518-11-01 00:03] wakes up',
+      '[1518-11-01 00:03] wakes up'
     ])).toEqual({ '10': { 'asleepAt': [undefined, 1, 1], 'sleepMinutes': 2 } })
     expect(guardReport([
       '[1518-11-01 00:00] Guard #10 begins shift',
       '[1518-11-01 00:01] falls asleep',
       '[1518-11-01 00:03] wakes up',
       '[1518-11-01 00:05] falls asleep',
-      '[1518-11-01 00:07] wakes up',
+      '[1518-11-01 00:07] wakes up'
     ])).toEqual({ '10': { 'asleepAt': [undefined, 1, 1, undefined, undefined, 1, 1], 'sleepMinutes': 4 } })
     expect(guardReport([
       '[1518-11-01 00:00] Guard #10 begins shift',
@@ -105,7 +125,7 @@ describe('find the sleepy guard', () => {
       '[1518-11-01 00:03] wakes up',
       '[1518-11-02 00:00] Guard #10 begins shift',
       '[1518-11-02 00:02] falls asleep',
-      '[1518-11-02 00:03] wakes up',
+      '[1518-11-02 00:03] wakes up'
     ])).toEqual({ '10': { 'asleepAt': [undefined, 1, 2], 'sleepMinutes': 3 } })
   })
   it('should find the sleepy guard', () => {
@@ -113,7 +133,7 @@ describe('find the sleepy guard', () => {
     expect(findSleepyGuard(guardReport([
       '[1518-11-01 00:00] Guard #10 begins shift',
       '[1518-11-01 00:01] falls asleep',
-      '[1518-11-01 00:02] wakes up',
+      '[1518-11-01 00:02] wakes up'
     ]))).toEqual({ mostASleep: 10, highestMinute: 1 })
 
     expect(findSleepyGuard(guardReport([
@@ -122,10 +142,19 @@ describe('find the sleepy guard', () => {
       '[1518-11-01 00:03] wakes up',
       '[1518-11-02 00:00] Guard #10 begins shift',
       '[1518-11-02 00:02] falls asleep',
-      '[1518-11-02 00:03] wakes up',
+      '[1518-11-02 00:03] wakes up'
     ]))).toEqual({ mostASleep: 10, highestMinute: 2 })
   })
   it('should calculate the solution', () => {
     expect(findSleepyGuard(guardReport(input))).toEqual({ mostASleep: 1777, highestMinute: 48 })
+  })
+})
+
+describe('find most frequent sleeper', () => {
+  it('should find them in the example', () => {
+    expect(findFrequentGuard(guardReport(samples))).toEqual({ mostFrequent: 99, highestMinute: 45 })
+  })
+  it('should calculate the solution', () => {
+    expect(findFrequentGuard(guardReport(input))).toEqual({ mostFrequent: 1889, highestMinute: 31 })
   })
 })
