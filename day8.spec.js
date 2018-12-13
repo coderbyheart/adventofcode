02c5a06b3f3d7f7026285d840817e16715104233
Markus Tacker <m@coderbyheart.com>
2018-12-13 23:12:37 +0100

    day 8, part 2

--- day8.spec.js
+++ day8.spec.js
@@ -4,17 +4,19 @@ const {readFileSync} = require('fs')
 
 /* global describe, it, expect, safeRegion  */
 
+const input = readFileSync('./day8.txt', 'utf-8');
+
 const parseTree = (sequence, parent, tree = []) => {
   const node = {
     childNodes: sequence.shift(),
     metadataEntries: sequence.shift(),
-    // parent,
+    children: [],
     metadata: []
   }
   tree.push(node)
   if (node.childNodes) {
     for (let i = 0; i < node.childNodes; i++) {
-      parseTree(sequence, node, tree)
+      node.children.push(parseTree(sequence, node, tree))
     }
   }
   if (node.metadataEntries) {
@@ -22,44 +24,58 @@ const parseTree = (sequence, parent, tree = []) => {
       node.metadata.push(sequence.shift())
     }
   }
-  return {
-    tree,
-    sequence
-  }
+  if (parent) return node
+  return tree
 }
 
-const metadataSum = sequence => parseTree(sequence.split(' ').map(n => +n)).tree.reduce((sum, node) => sum + node.metadata.reduce((sum, v) => sum + v, 0), 0)
+const metadataSum = sequence => parseTree(sequence.split(' ').map(n => +n)).reduce((sum, node) => sum + node.metadata.reduce((sum, v) => sum + v, 0), 0)
+
+const rootNodeSum = sequence => {
+  const tree = parseTree(sequence.split(' ').map(n => +n))
+  return summarizeNode(tree[0])
+}
+
+const summarizeNode = (node) => {
+  if (node.childNodes === 0) {
+    return node.metadata.reduce((sum, v) => sum + v, 0)
+  }
+  return node.metadata.reduce((sum, i) => {
+    if (i === 0) return sum
+    if (!node.children[i - 1]) return sum
+    return sum + summarizeNode(node.children[i - 1])
+  }, 0)
+}
 
 describe('tree parser', () => {
   it('should parse test sequences', () => {
-    const {tree} = parseTree('0 0'.split(' ').map(n => +n))
+    const tree = parseTree('0 0'.split(' ').map(n => +n))
     expect(tree).toHaveLength(1)
   })
   it('should parse test sequences', () => {
-    const {tree} = parseTree('0 1 2'.split(' ').map(n => +n))
+    const tree = parseTree('0 1 2'.split(' ').map(n => +n))
     expect(tree).toHaveLength(1)
   })
   it('should parse test sequences', () => {
-    const {tree} = parseTree('1 0 0 0 1 2'.split(' ').map(n => +n))
+    const tree = parseTree('1 0 0 0 1 2'.split(' ').map(n => +n))
     expect(tree).toHaveLength(2)
   })
   it('should parse test sequences', () => {
-    const {tree} = parseTree('1 0 1 0 0 0 1 2'.split(' ').map(n => +n))
+    const tree = parseTree('1 0 1 0 0 0 1 2'.split(' ').map(n => +n))
     expect(tree).toHaveLength(3)
   })
   it('should parse test sequences', () => {
-    const {tree} = parseTree('1 1 0 0 4 1 2'.split(' ').map(n => +n))
+    const tree = parseTree('1 1 0 0 4 1 2'.split(' ').map(n => +n))
     expect(tree).toHaveLength(2)
     expect(tree[0].metadata).toEqual([4])
   })
   it('should parse test sequences', () => {
-    const {tree} = parseTree('1 1 0 2 5 6 4 1 2'.split(' ').map(n => +n))
+    const tree = parseTree('1 1 0 2 5 6 4 1 2'.split(' ').map(n => +n))
     expect(tree).toHaveLength(2)
     expect(tree[0].metadata).toEqual([4])
     expect(tree[1].metadata).toEqual([5, 6])
   })
   it('should parse the example sequence', () => {
-    const {tree} = parseTree('2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'.split(' ').map(n => +n))
+    const tree = parseTree('2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'.split(' ').map(n => +n))
     expect(tree).toHaveLength(4)
   })
 
@@ -69,6 +85,15 @@ describe('metadataSum', () => {
     expect(metadataSum('2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2')).toEqual(138)
   })
   it('should sum the puzzle sequence', () => {
-    expect(metadataSum(readFileSync('./day8.txt', 'utf-8'))).toEqual(138)
+    expect(metadataSum(input)).toEqual(36627)
+  })
+})
+
+describe('rootNodeSum', () => {
+  it('should calculate the example sequence', () => {
+    expect(rootNodeSum('2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2')).toEqual(66)
+  })
+  it('should calculate the puzzle sequence', () => {
+    expect(rootNodeSum(input)).toEqual(16695)
   })
 })
