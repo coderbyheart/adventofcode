0ee6096a995e4f957974d29771bc322180a1bd98
Markus Tacker <m@coderbyheart.com>
2019-12-01 16:22:46 +0100

    feat(day1): part 2

--- day1.spec.ts
+++ day1.spec.ts
@@ -1,4 +1,4 @@
-import { moduleLaunchFuel, modulesLaunchFuel } from "./moduleLaunchFuel"
+import { moduleLaunchFuel, modulesLaunchFuel, moduleLaunchFuelWithExtraFuelForFuel, modulesLaunchFuelWithExtraFuelForFuel } from "./moduleLaunchFuel"
 import { fileToArray } from "./utils/fileToArray"
 
 describe('Fuel Counter-Upper needs to determined the amount of fuel required', () => {
@@ -15,5 +15,18 @@ describe('Fuel Counter-Upper needs to determined the amount of fuel required', (
     it('should calculate the required fuel for all modules', async () => {
         const modules = await fileToArray<number>('day1.input.txt', s => parseInt(s, 10))
         expect(modulesLaunchFuel(modules)).toEqual(3421505)
+    })
+    describe('fuel for fuel needs to be calculated', () => {
+        test.each([
+            [14, 2],
+            [1969, 966],
+            [100756, 50346]
+        ])(`mass of %i needs %i fuel`, (mass, expectedFuel) => {
+            expect(moduleLaunchFuelWithExtraFuelForFuel(mass)).toEqual(expectedFuel)
+        })
+    })
+    it('should calculate the required fuel for all modules including the extra fuel', async () => {
+        const modules = await fileToArray<number>('day1.input.txt', s => parseInt(s, 10))
+        expect(modulesLaunchFuelWithExtraFuelForFuel(modules)).toEqual(5129386)
     })
 })
--- moduleLaunchFuel.ts
+++ moduleLaunchFuel.ts
@@ -10,3 +10,29 @@ export const moduleLaunchFuel = (mass: number): number => Math.floor(mass / 3) -
  * Calculates the launch fuel for all given modules
  */
 export const modulesLaunchFuel = (modules: number[]) => modules.reduce((totalMass, mass) => totalMass + moduleLaunchFuel(mass), 0)
+
+/**
+ * Caculate the total fuel required for a module
+ * 
+ * Calculate its fuel and add it to the total.
+ * Then, treat the fuel amount you just calculated as the input mass 
+ * and repeat the process, 
+ * continuing until a fuel requirement is zero or negative.
+ */
+export const moduleLaunchFuelWithExtraFuelForFuel = (mass: number) => {
+    let fuelAmount = moduleLaunchFuel(mass)
+    let total = fuelAmount
+    do {
+        const additionalFuel = moduleLaunchFuel(fuelAmount)
+        fuelAmount = additionalFuel
+        if (additionalFuel > 0) {
+            total += additionalFuel
+        }
+    } while (fuelAmount > 0)
+    return total
+}
+
+/**
+ * Caculate the total fuel required for all given modules
+ */
+export const modulesLaunchFuelWithExtraFuelForFuel = (modules: number[]) => modules.reduce((totalMass, mass) => totalMass + moduleLaunchFuelWithExtraFuelForFuel(mass), 0)
