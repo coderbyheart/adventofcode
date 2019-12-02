329b79aeb0b261068024eb39d323dd7046c4393b
Markus Tacker <m@coderbyheart.com>
2019-12-02 08:28:18 +0100

    refactor: generalize instructions

--- intcode.ts
+++ intcode.ts
@@ -1,18 +1,30 @@
+const add = (sequence: number[], pos: number): number => {
+    const noun = sequence[pos + 1]
+    const verb = sequence[pos + 2]
+    const out = sequence[pos + 3]
+    sequence[out] = sequence[noun] + sequence[verb]
+    return 4
+}
+
+const mul = (sequence: number[], pos: number): number => {
+    const noun = sequence[pos + 1]
+    const verb = sequence[pos + 2]
+    const out = sequence[pos + 3]
+    sequence[out] = sequence[noun] * sequence[verb]
+    return 4
+}
+
+const instructions = {
+    1: add,
+    2: mul
+}
+
 export const computeSequence = (sequence: number[], pos: number = 0): number[] => {
     const op = sequence[pos]
     switch (op) {
         case 1:
-            const a1 = sequence[pos + 1]
-            const a2 = sequence[pos + 2]
-            const out = sequence[pos + 3]
-            sequence[out] = sequence[a1] + sequence[a2]
-            return computeSequence(sequence, pos + 4)
         case 2:
-            const m1 = sequence[pos + 1]
-            const m2 = sequence[pos + 2]
-            const out2 = sequence[pos + 3]
-            sequence[out2] = sequence[m1] * sequence[m2]
-            return computeSequence(sequence, pos + 4)
+            return computeSequence(sequence, pos + instructions[op](sequence, pos))
         case 99:
             return sequence
         default:
