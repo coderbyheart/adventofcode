78439fa3cc292bc0be33e08397ac36b4e4e516a5
Markus Tacker <Markus.Tacker@NordicSemi.no>
2019-12-05 11:55:04 +0100

    feat(day5): solution 2

--- day5/day5.solution.spec.ts
+++ day5/day5.solution.spec.ts
@@ -7,12 +7,26 @@ describe('Day 5: Part 1', () => {
     it('should calculate the solution', () => {
         const outputs = [] as number[]
         computeSequence({
-            sequence,
+            sequence: [...sequence],
             input: 1,
             output: out => {
                 outputs.push(out)
             },
         })
         expect(outputs.pop()).toEqual(13210611)
+    })
+})
+
+describe('Day 5: Part 2', () => {
+    it('should calculate the solution', () => {
+        const outputs = [] as number[]
+        computeSequence({
+            sequence: [...sequence],
+            input: 5,
+            output: out => {
+                outputs.push(out)
+            },
+        })
+        expect(outputs.pop()).toEqual(584126)
     })
 })
