8933dd81d35e3cbfa8a8ba7e695bacf5dc6270ef
Markus Tacker <m@coderbyheart.com>
2019-12-07 19:40:59 +0100

    feat(day7): tests

---
+++ day7/parseParameter.ts
@@ -0,0 +1,32 @@
+export enum ParameterMode {
+	POSITION = 0,
+	IMMEDIATE = 1,
+}
+
+export const parseParameter = (
+	parameter: number,
+): { op: number; modes: ParameterMode[] } => {
+	const s = parameter.toString()
+
+	return {
+		// The opcode is a two-digit number based only on the ones and tens digit of the value,
+		// that is, the opcode is the rightmost two digits of the first value in an instruction.
+		op: parseInt(s.substr(-2), 10),
+		// Parameter modes are single digits, one per parameter, read right-to-left from the opcode:
+		// the first parameter's mode is in the hundreds digit,
+		// the second parameter's mode is in the thousands digit,
+		// the third parameter's mode is in the ten-thousands digit, and so on.
+		modes: s
+			.substr(0, s.length - 2)
+			.split('')
+			.map(s => {
+				const m = parseInt(s, 10)
+				if (m !== 0 && m !== 1) {
+					throw new Error(`Invalid parameter mode: ${s}`)
+				}
+				return m
+			})
+			.reverse(),
+		// Any missing modes are 0. -> these are blank in the modes array
+	}
+}
