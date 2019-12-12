5e7150cbb66ab40f0f7c7828074294a2ec048520
Markus Tacker <m@coderbyheart.com>
2019-12-12 09:44:11 +0100

    feat(day12): implement energy calculation

---
+++ day12/calculateTotalEnergyOfSystem.ts
@@ -0,0 +1,22 @@
+type Position3D = [
+    number,
+    number,
+    number,
+]
+
+type Moon = {
+    pos: Position3D
+    vel: Position3D
+}
+
+const sumAbsolute = (v: number[]) => v.reduce((sum, n) => sum + Math.abs(n), 0)
+
+/**
+ * The total energy for a single moon is its potential energy multiplied by its kinetic energy. 
+ * A moon's potential energy is the sum of the absolute values of its x, y, and z position coordinates. 
+ * A moon's kinetic energy is the sum of the absolute values of its velocity coordinates.
+ */
+const calculateTotalEnergyOfMoon = ({ pos, vel }: Moon) => sumAbsolute(pos) * sumAbsolute(vel)
+
+export const calculateTotalEnergyOfSystem = (galaxy: Moon[]): number => galaxy
+    .reduce((totalEnergy, moon) => totalEnergy + calculateTotalEnergyOfMoon(moon), 0)
