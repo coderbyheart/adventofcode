8763d1e29f6e8cb0e702b0bd147e5cead5958656
Markus Tacker <m@coderbyheart.com>
2019-12-12 09:34:16 +0100

    test(da12): added examples

---
+++ day12/calculateTotalEnergy.spec.ts
@@ -0,0 +1,18 @@
+describe('calculateTotalEnergy', () => {
+    it('should calculate the total energy of the first example', () => {
+        expect(calculateTotalEnergy(
+            { pos: [2, 1, -3], vel: [-3, -2, 1] },
+            { pos: [1, -8, 0], vel: [-1, 1, 3] },
+            { pos: [3, -6, 1], vel: [3, 2, -3] },
+            { pos: [2, 0, 4], vel: [1, -1, -1] },
+        )).toEqual(179)
+    })
+    it('should calculate the total energy of the second example', () => {
+        expect(calculateTotalEnergy(
+            { pos: [8, -12, -9], vel: [-7, 3, 0] },
+            { pos: [13, 16, -3], vel: [3, -11, -5] },
+            { pos: [-29, -11, -1], vel: [-3, 7, 4] },
+            { pos: [16, -13, 23], vel: [7, 1, 1] },
+        )).toEqual(1940)
+    })
+})
