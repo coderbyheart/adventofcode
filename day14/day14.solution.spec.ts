a0bf5f7a388a99f9fbf5341d33fb460293e1aa4a
Markus Tacker <m@coderbyheart.com>
2019-12-14 12:55:05 +0100

    feat(day14): part 1 solution

---
+++ day14/day14.solution.spec.ts
@@ -0,0 +1,14 @@
+import { nanofactory } from './nanofactory'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const recipes = fs.readFileSync(
+	path.resolve(process.cwd(), 'day14/input.txt'),
+	'utf-8',
+)
+
+describe('Day 14: Part 1', () => {
+	it('should calculate the solution', () => {
+		expect(nanofactory(recipes)).toEqual(168046)
+	})
+})
