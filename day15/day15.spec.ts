71c2ba0d6638f998c7dc506e9dfd3fc48e0c421a
Markus Tacker <Markus.Tacker@NordicSemi.no>
2019-12-16 11:56:36 +0100

    feat(day15): part 2

--- day15/day15.spec.ts
+++ day15/day15.spec.ts
@@ -1,5 +1,6 @@
 import { fileToArray } from '../utils/fileToArray'
-import { repairRobot } from './repairRobot'
+import { repairRobot, findOxygenSystem, drawMap } from './repairRobot'
+import { floodFill, Tile } from './floodFill'
 
 const program = fileToArray('day15/input.txt', s =>
 	s.split(',').map(s => parseInt(s, 10)),
@@ -8,5 +9,21 @@ const program = fileToArray('day15/input.txt', s =>
 describe('Day 15: Part 1', () => {
 	it('should find the solution', async () => {
 		expect(await repairRobot([...program])).toEqual(246)
+	})
+})
+
+describe('Day 15: Part 2', () => {
+	it('should find the solution', async () => {
+		expect(await repairRobot([...program])).toEqual(246)
+		const r1 = await findOxygenSystem([...program])
+		// Because the implementation favours unvisited fields, this maze will be fully explored
+		const r2 = await findOxygenSystem([...program], r1.usedMap)
+		await drawMap(r2.usedMap)
+		const floodMap = r2.map.map(row =>
+			row.map(b => (b ? Tile.FLOODABLE : Tile.WALL)),
+		) as number[][]
+		floodMap[r2.oxygenSystemPosition[1]][r2.oxygenSystemPosition[0]] =
+			Tile.FLOODED
+		expect(await floodFill(floodMap)).toEqual(376)
 	})
 })
--- day15/part2.ts
+++ day15/part2.ts
@@ -1,5 +1,6 @@
 import { fileToArray } from '../utils/fileToArray'
 import { findOxygenSystem, drawMap } from './repairRobot'
+import { floodFill, Tile, drawMap as drawFloodMap } from './floodFill'
 
 const program = fileToArray('day15/input.txt', s =>
 	s.split(',').map(s => parseInt(s, 10)),
@@ -10,10 +11,14 @@ const main = async () => {
 	// Because the implementation favours unvisited fields, this maze will be fully explored
 	const r2 = await findOxygenSystem([...program], r1.usedMap)
 	await drawMap(r2.usedMap)
-	const floodMap = r2.map.map(row => row.map(b => (b ? 1 : 0))) as number[][]
-	floodMap[r2.oxygenSystemPosition[1]][r2.oxygenSystemPosition[0]] = 2
-	console.log(floodMap.map(row => row.join('')).join('\n'))
-	// floodFill(r2.map, r2.oxygenSystemPosition)
+	const floodMap = r2.map.map(row =>
+		row.map(b => (b ? Tile.FLOODABLE : Tile.WALL)),
+	) as number[][]
+	floodMap[r2.oxygenSystemPosition[1]][r2.oxygenSystemPosition[0]] =
+		Tile.FLOODED
+	await floodFill(floodMap, async (map, iteration) =>
+		drawFloodMap(map, iteration, true),
+	)
 }
 
 main()
