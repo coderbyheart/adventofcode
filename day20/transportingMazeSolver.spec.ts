ebecb701f8c9979bd72404f4752332fa029b2ea3
Markus Tacker <m@coderbyheart.com>
2019-12-20 11:05:37 +0100

    feat(day20): example 2

--- day20/transportingMazeSolver.spec.ts
+++ day20/transportingMazeSolver.spec.ts
@@ -7,13 +7,23 @@ import * as fs from 'fs'
 import * as path from 'path'
 
 describe('Transporting maze solver', () => {
-	it('should solve the example', () => {
+	it('should solve the first example', () => {
 		const example = fs.readFileSync(
 			path.resolve(process.cwd(), 'day20/example1.txt'),
 			'utf-8',
 		)
 		const res = transportingMazeSolver(example)
 		expect(res?.path).toHaveLength(23)
+		drawSolution(example, res as Location)
+	})
+
+	it('should solve the second example', () => {
+		const example = fs.readFileSync(
+			path.resolve(process.cwd(), 'day20/example2.txt'),
+			'utf-8',
+		)
+		const res = transportingMazeSolver(example)
+		expect(res?.path).toHaveLength(58)
 		drawSolution(example, res as Location)
 	})
 })
--- day20/transportingMazeSolver.ts
+++ day20/transportingMazeSolver.ts
@@ -165,15 +165,23 @@ export const transportingMazeSolver = (maze: string): Location | undefined => {
 	return undefined
 }
 
+const split = (s: string, length: number) => {
+	const ret = []
+	for (let offset = 0, strLen = s.length; offset < strLen; offset += length) {
+		ret.push(s.slice(offset, length + offset))
+	}
+	return ret
+}
+
 export const drawSolution = (maze: string, finalLocation: Location) => {
 	const width = maze.indexOf('\n')
 	const mapAsString = maze.trimEnd().replace(/\n/g, '')
 	let solution = mapAsString
-	finalLocation.path.map(p => {
+	finalLocation.path.forEach(p => {
 		solution =
 			solution.substr(0, p.y * width + p.x) +
 			'@' +
 			solution.substr(p.y * width + p.x + 1)
 	})
-	console.log(new RegExp(`.{${width}}`, 'g').exec(solution)?.join('\n'))
+	console.log(split(solution, width).join('\n'))
 }
