a3afa0d3eb16dc4727c25fc00a57178582f75183
Markus Tacker <m@coderbyheart.com>
2019-12-22 11:28:36 +0100

    feat(day22): cut -4

--- day22/cardshuffle.spec.ts
+++ day22/cardshuffle.spec.ts
@@ -20,4 +20,7 @@ describe('Card Shuffle', () => {
 	test('cut 3', () => {
 		expect(cut(3)([...testDeck])).toEqual([3, 4, 5, 6, 7, 8, 9, 0, 1, 2])
 	})
+	test('cut -4', () => {
+		expect(cut(-4)([...testDeck])).toEqual([6, 7, 8, 9, 0, 1, 2, 3, 4, 5])
+	})
 })
