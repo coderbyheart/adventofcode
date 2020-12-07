a9e2cc1f8cf400ad820ca980a391037a39b57e8b
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-12-07 09:31:57 +0100

    feat(day07): part 2

--- day07/solution.spec.ts
+++ day07/solution.spec.ts
@@ -1,4 +1,5 @@
 import { loader, loadString } from '../lib/loader'
+import { countBags } from './countBags'
 import { packBag } from './packBag'
 import { parseRules } from './parseRules'
 
@@ -19,6 +20,14 @@ vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.`
 
+const sample2 = `shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`
+
 describe('Day 7: Handy Haversacks', () => {
 	describe('Part 1', () => {
 		it('should solve the sample', () => {
@@ -34,8 +43,24 @@ describe('Day 7: Handy Haversacks', () => {
 		it('should solve', () => {
 			const rules = parseRules(input)
 			const packager = packBag(rules)
-			const bags = packager('shiny gold')
-			expect(bags).toHaveLength(378)
+			expect(packager('shiny gold')).toHaveLength(378)
+		})
+	})
+	describe('Part 2', () => {
+		it('should solve the sample', () => {
+			const rules = parseRules(loadString(sample))
+			const counter = countBags(rules)
+			expect(counter('shiny gold')).toEqual(32)
+		})
+		it('should solve the second sample', () => {
+			const rules = parseRules(loadString(sample2))
+			const counter = countBags(rules)
+			expect(counter('shiny gold')).toEqual(126)
+		})
+		it('should solve', () => {
+			const rules = parseRules(input)
+			const counter = countBags(rules)
+			expect(counter('shiny gold')).toEqual(27526)
 		})
 	})
 })
