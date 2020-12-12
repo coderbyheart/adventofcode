e31c0f050475b3cffddab6d24dd969807ff4a891
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-12-12 11:21:34 +0100

    feat(day12): part 2

--- day12/navigate.ts
+++ day12/navigate.ts
@@ -8,8 +8,13 @@ type Position = { x: number; y: number }
 
 const instructionRx = /^(?<dir>N|S|E|W|L|R|F)(?<amount>[0-9]+)$/
 
-const headingToDirection = (heading: number): Direction => {
-	switch (heading % 360) {
+/**
+ * Translates the heading to a direction
+ *
+ * Note: Turn instructions are only given in 90 degree steps.
+ */
+const headingToDirection = (headingInDegrees: number): Direction => {
+	switch (headingInDegrees % 360) {
 		case 0:
 			return Direction.N
 		case 90:
@@ -22,11 +27,15 @@ const headingToDirection = (heading: number): Direction => {
 		case -180:
 			return Direction.S
 		default:
-			console.warn(`Unknown heading: ${heading % 360}`)
+			console.warn(`Unknown heading: ${headingInDegrees % 360}`)
 			return Direction.N
 	}
 }
 
+/**
+ * Move the given position in the given direction.
+ * pos is modified in place.
+ */
 const move = (pos: Position) => (dir: Direction, n: number) => {
 	switch (dir) {
 		case Direction.N:
@@ -44,14 +53,78 @@ const move = (pos: Position) => (dir: Direction, n: number) => {
 	}
 }
 
-export const navigate = (
-	instructions: string[],
-	startDirection = 90,
-): Position => {
-	const pos = { x: 0, y: 0 }
-	const mv = move(pos)
-	let heading = startDirection
+/**
+ * Convert degrees to radians
+ */
+const degreesToRadians = (deg: number): number => (deg * Math.PI) / 180
 
+/**
+ * Rotate the given position for the given angle around 0,0
+ * pos is modified in place.
+ */
+const rotate = (pos: Position) => (angleInDegrees: number) => {
+	const s = Math.sin(degreesToRadians(angleInDegrees))
+	const c = Math.cos(degreesToRadians(angleInDegrees))
+
+	const newX = pos.x * c - pos.y * s
+	const newY = pos.x * s + pos.y * c
+
+	pos.x = Math.round(newX)
+	pos.y = Math.round(newY)
+}
+
+/**
+ * Implements the relative navigation where instructions are applied relative
+ * to the waypoint
+ */
+export const navigateRelative = (
+	instructions: string[],
+	waypoint: Position,
+): Position => {
+	const shipPosition = { x: 0, y: 0 }
+	navigate({
+		instructions,
+		moveTo: move(waypoint),
+		changeDir: rotate(waypoint),
+		forward: (n: number) => {
+			shipPosition.x += waypoint.x * n
+			shipPosition.y += waypoint.y * n
+		},
+	})
+	return shipPosition
+}
+
+/**
+ * Implements the absolute navigation where instructions are applied to the ship
+ */
+export const navigateAbsolute = (
+	instructions: string[],
+	shipHeading = 90,
+): Position => {
+	const shipPosition = { x: 0, y: 0 }
+	const moveShip = move(shipPosition)
+	navigate({
+		instructions,
+		forward: (n: number) => moveShip(headingToDirection(shipHeading), n),
+		moveTo: moveShip,
+		changeDir: (n: number) => {
+			shipHeading += n
+		},
+	})
+	return shipPosition
+}
+
+const navigate = ({
+	instructions,
+	moveTo,
+	changeDir,
+	forward,
+}: {
+	instructions: string[]
+	moveTo: (dir: Direction, amount: number) => void
+	changeDir: (amount: number) => void
+	forward: (amount: number) => void
+}): void => {
 	instructions.forEach((s) => {
 		const m = instructionRx.exec(s)
 		if (m === null) {
@@ -61,27 +134,26 @@ export const navigate = (
 		const n = parseInt(amount, 10)
 		switch (dir) {
 			case 'N':
-				mv(Direction.N, n)
+				moveTo(Direction.N, n)
 				break
 			case 'S':
-				mv(Direction.S, n)
+				moveTo(Direction.S, n)
 				break
 			case 'E':
-				mv(Direction.E, n)
+				moveTo(Direction.E, n)
 				break
 			case 'W':
-				mv(Direction.W, n)
+				moveTo(Direction.W, n)
 				break
 			case 'L':
-				heading -= n
+				changeDir(-n)
 				break
 			case 'R':
-				heading += n
+				changeDir(n)
 				break
 			case 'F':
-				mv(headingToDirection(heading), n)
+				forward(n)
 				break
 		}
 	})
-	return pos
 }
--- day12/solution.spec.ts
+++ day12/solution.spec.ts
@@ -1,26 +1,40 @@
 import { loader, loadString } from '../lib/loader'
-import { navigate } from './navigate'
+import { navigateAbsolute, navigateRelative } from './navigate'
 
 const load = loader(12)
+const sample = loadString(
+	`F10
+	N3
+	F7
+	R90
+	F11`,
+)
 const input = load('input')
+
+const manhatten = (
+	{ x: x1, y: y1 }: { x: number; y: number },
+	{ x: x2, y: y2 } = { x: 0, y: 0 },
+): number => Math.abs(x1 - x2) + Math.abs(y1 - y2)
 
 describe('Day 12: Rain Risk', () => {
 	describe('Part 1', () => {
 		it('should solve the sample', () => {
-			const { x, y } = navigate(
-				loadString(
-					`F10
-					N3
-					F7
-					R90
-					F11`,
-				),
-			)
-			expect(Math.abs(x) + Math.abs(y)).toEqual(25)
+			const pos = navigateAbsolute(sample, 90)
+			expect(manhatten(pos)).toEqual(25)
 		})
 		it('should solve', () => {
-			const { x, y } = navigate(input)
-			expect(Math.abs(x) + Math.abs(y)).toEqual(441)
+			const pos = navigateAbsolute(input, 90)
+			expect(manhatten(pos)).toEqual(441)
+		})
+	})
+	describe('Part 2', () => {
+		it('should solve the sample', () => {
+			const pos = navigateRelative(sample, { x: 10, y: -1 })
+			expect(manhatten(pos)).toEqual(286)
+		})
+		it('should solve', () => {
+			const pos = navigateRelative(input, { x: 10, y: -1 })
+			expect(manhatten(pos)).toEqual(40014)
 		})
 	})
 })
