a6bd51ff2893ea7e7db252a855784ab1fc018264
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-12-17 13:38:22 +0100

    feat(day16): part 2 try 3

--- day16/test2.ts
+++ day16/test2.ts
@@ -1,71 +1,105 @@
-import { loader } from '../lib/loader'
+import { loadString } from '../lib/loader'
 import { parseRules } from './isValidTicket'
-
-const load = loader(16)
-const tickets = load('tickets').map((s) =>
-	s.split(',').map((s) => parseInt(s, 10)),
-)
-const rules = parseRules(load('rules'))
 
 type Rules = [string, [[number, number], [number, number]]][]
 
-const departureRules: Rules = Object.entries(rules).filter(([k]) =>
-	k.startsWith('departure'),
-)
+const rulePositionChecked = {} as Record<string, boolean>
+const ruleOrderChecked = {} as Record<string, boolean>
 
 const findMatch = (
 	rules: Rules,
 	tickets: number[][],
-	parents: Record<string, number> = {},
+	remainingRules = rules,
+	parents: string[] = [],
 ): any => {
-	if (rules.length === 0) {
-		// Check all tickets
-		for (const ticket of tickets) {
-			for (const [ruleName, k] of Object.entries(parents)) {
+	if (remainingRules.length === 0) {
+		if (rules.length !== parents.length) {
+			console.error({
+				remainingRules,
+				parents,
+			})
+			throw new Error('BUG!')
+		}
+		if (ruleOrderChecked[parents.join(',')] !== undefined) return
+		// Check all rules with all tickets
+		for (let k = 0; k < parents.length; k++) {
+			const ruleName = parents[k]
+			const key = `${ruleName}-${k}`
+			if (rulePositionChecked[key] !== undefined) return // fail early
+			const rule = rules.find(([name]) => name === ruleName)
+			const [ruleMin1, ruleMax1] = rule?.[1][0] ?? [
+				Number.MAX_SAFE_INTEGER,
+				-Number.MAX_SAFE_INTEGER,
+			]
+			const [ruleMin2, ruleMax2] = rule?.[1][1] ?? [
+				Number.MAX_SAFE_INTEGER,
+				-Number.MAX_SAFE_INTEGER,
+			]
+			for (const ticket of tickets) {
 				const v = ticket[k]
-				const rule = rules.find(([name]) => name === ruleName)
-				const [ruleMin1, ruleMax1] = rule?.[1][0] ?? [
-					Number.MAX_SAFE_INTEGER,
-					-Number.MAX_SAFE_INTEGER,
-				]
-				const [ruleMin2, ruleMax2] = rule?.[1][1] ?? [
-					Number.MAX_SAFE_INTEGER,
-					-Number.MAX_SAFE_INTEGER,
-				]
 				if (
 					!(
-						(v >= ruleMin1 || v >= ruleMin2) &&
-						(v <= ruleMax1 || v <= ruleMax2)
+						(v >= ruleMin1 && v <= ruleMax1) ||
+						(v >= ruleMin2 && v <= ruleMax2)
 					)
 				) {
+					ruleOrderChecked[parents.join(',')] = false
 					return // fail
 				}
 			}
 		}
-		console.log(parents)
-		return // pass
+		ruleOrderChecked[parents.join(',')] = true
+		return parents // pass
 	}
-	for (let r = 0; r < rules.length; r++) {
-		const rule = rules[r]
-		const rest = [...rules.slice(0, r), ...rules.slice(r + 1)]
+	for (let r = 0; r < remainingRules.length; r++) {
+		const rule = remainingRules[r]
+		const rest = [...remainingRules.slice(0, r), ...remainingRules.slice(r + 1)]
 		for (let k = 0; k < tickets[0].length; k++) {
-			if (Object.values(parents).includes(k)) continue
+			const key = `${rule[0]}-${k}`
+			if (rulePositionChecked[key] !== undefined) return
 			const [ruleMin1, ruleMax1] = rule[1][0]
 			const [ruleMin2, ruleMax2] = rule[1][1]
+			let allMatch = true
 			for (const ticket of tickets) {
 				const v = ticket[k]
 				if (
-					(v >= ruleMin1 || v >= ruleMin2) &&
-					(v <= ruleMax1 || v <= ruleMax2)
+					!(
+						(v >= ruleMin1 && v <= ruleMax1) ||
+						(v >= ruleMin2 && v <= ruleMax2)
+					)
 				) {
-					findMatch(rest, tickets, {
-						...parents,
-						[rule[0]]: k,
-					})
+					allMatch = false
+					break
 				}
+			}
+			if (allMatch) {
+				const newParents = [...parents]
+				newParents[k] = rule[0]
+				return findMatch(rules, tickets, rest, newParents)
+			} else {
+				rulePositionChecked[key] = false
 			}
 		}
 	}
 }
 
-console.log(findMatch(departureRules, tickets))
+/*
+const load = loader(16)
+const tickets = load('tickets').map((s) =>
+	s.split(',').map((s) => parseInt(s, 10)),
+)
+const rules = parseRules(load('rules'))
+console.log(findMatch(Object.entries(rules), tickets))
+console.log(rules)
+*/
+
+const step2sampleTickets = loadString(`
+3,9,18
+15,1,5
+5,14,9`).map((s) => s.split(',').map((s) => parseInt(s, 10)))
+const sample2Rules = parseRules(
+	loadString(`class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19`),
+)
+console.log(findMatch(Object.entries(sample2Rules), step2sampleTickets)) // 'row', 'class', 'seat',
