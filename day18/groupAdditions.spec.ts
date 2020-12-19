1871ee193842c4dd3593490c23f13b00c9cad5f2
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-12-19 15:48:35 +0100

    WIP

--- day18/groupAdditions.spec.ts
+++ day18/groupAdditions.spec.ts
@@ -1,17 +1,58 @@
-import { toExpression } from './toExpression'
-import { tokenize } from './tokenize'
+import { isNumber } from './calc'
+import { calcTokens } from './calcTokens'
 
-describe.skip('groupAdditions', () => {
+const reverse = (s: string): string => s.split('').reverse().join('')
+
+const addOpeningParen = (exp: string): string => {
+	let rev = reverse(exp)
+	let len = rev.length
+	let parenCount = 0
+	for (let i = 0; i < len; i++) {
+		const c = rev[i]
+		if (c === ')') parenCount++
+		if (c === '(') parenCount++
+		if (isNumber(c)) {
+			rev = `${rev}(`
+			i++
+			len++
+		}
+	}
+	return reverse(rev)
+}
+const addClosingParen = (exp: string): string => exp
+
+const groupAdditions = (expression: string): string => {
+	console.log(expression)
+	let res = expression
+	for (let i = 0; i < expression.length; i++) {
+		if (res[i] === '+') {
+			const left = addOpeningParen(res.substr(0, i - 1))
+			const right = addClosingParen(res.substr(i + 2))
+			console.log({ left, right })
+			res = `${left} + ${right}`
+		}
+	}
+	return res
+}
+
+describe('groupAdditions', () => {
 	it.each([
-		['1 + 2', '(1 + 2)'],
-		['1 + 2 * 3', '(1 + 2) * 3'],
-		['1 + 2 * 3 + 4 * 5 + 6', '(1 + 2) * (3 + 4) * (5 + 6)'],
-		//['1 + (2 * 3) + (4 * (5 + 6))', 51],
-		//['2 * 3 + (4 * 5)', 46],
-		//['5 + (8 * 3 + 9 + 3 * 4 * 3)', 1445],
-		//['5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', 669060],
-		//['((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', 23340],
-	])(`should group %s to %s`, (expression, expected) =>
-		expect(toExpression(tokenize(expression))).toEqual(expected),
-	)
+		['1 + 2 * 3 + 4 * 5 + 6', '(1 + 2) * (3 + 4) * (5 + 6)', 231],
+		//['1 + (2 * 3) + (4 * (5 + 6))', '((1 + (2 * 3)) + (4 * (5 + 6)))', 51],
+		//['2 * 3 + (4 * 5)', '2 * (3 + (4 * 5))', 46],
+		//['5 + (8 * 3 + 9 + 3 * 4 * 3)', '(5 + (8 * ((3 + 9) + 3) * 4 * 3))', 1445],
+		//[
+		//	'5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
+		//		'5 * 9 * (7 * 3 * (3 + 9) * (3 + ((8 + 6) * 4)))',
+		//		669060,
+		//	],
+		//	[
+		//		'((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2',
+		//		'(((((2 + 4) * 9) * (((6 + 9) * (8 + 6)) + 6)) + 2) + 4) * 2',
+		//		23340,
+		//	],
+	])(`should group %s to %s and equal %d`, (expression, escaped, expected) => {
+		expect(groupAdditions(expression)).toEqual(escaped)
+		expect(calcTokens(groupAdditions(expression))).toEqual(expected)
+	})
 })
