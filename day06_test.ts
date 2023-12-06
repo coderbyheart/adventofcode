4f1eb4e27e54f0709099bed9f5536fe44a448004
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-12-06 10:45:26 +0100

    feat(day06): Part 1

---
+++ day06_test.ts
@@ -0,0 +1,48 @@
+import { assertEquals } from "https://deno.land/std@0.208.0/assert/assert_equals.ts";
+
+Deno.test("Day 6: Wait For It", async (t) => {
+  await t.step("Part 1", async (t) => {
+    await t.step(`Solve the example`, () =>
+      assertEquals(
+        waysToWin([
+          [7, 9],
+          [15, 40],
+          [30, 200],
+        ]),
+        288
+      )
+    );
+    await t.step(`Solve`, () =>
+      assertEquals(
+        waysToWin([
+          [47, 282],
+          [70, 1079],
+          [75, 1147],
+          [66, 1062],
+        ]),
+        281600
+      )
+    );
+  });
+});
+
+const waysToWin = (races: Array<[number, number]>): number => {
+  const ways: Array<number> = [];
+
+  for (const [time, distance] of races) {
+    const winningTimes: Array<number> = [];
+    let lastDistance = 0;
+    for (let i = 1; i <= time; i++) {
+      const currentDistance = i * (time - i);
+      // Abort if distance is getting smaller and will no longer win
+      if (currentDistance < lastDistance && currentDistance < distance) break;
+      lastDistance = currentDistance;
+      if (currentDistance > distance) winningTimes.push(i);
+    }
+    ways.push(winningTimes.length);
+  }
+
+  return ways.reduce(mul);
+};
+
+const mul = (total: number, n: number) => total * n;
